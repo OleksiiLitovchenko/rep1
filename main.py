bc08c977e5b3c9960ab74e17da334851df576b1f
Oleksii <aleksejlitovcenko69@gmail.com>
2023-06-24 11:02:47 +0300

    add function total_expenses

--- main.py
+++ main.py
@@ -9,7 +9,17 @@ def total_expenses_by_category(filename: str):
             else:
                 res[category] = money
     return res
+def total_expenses(filename: str):
+    res = 0
+    with open(filename, encoding='utf-8') as file:
+        for line in file:
+            *_, money, category = line.strip().split()
+            money = float(money.replace('$', ''))
+            res += money
+    return res
+
 
 
 if __name__ == '__main__':
     print(total_expenses_by_category('hw_10_test.txt'))
+    print(total_expenses('hw_10_test.txt'))
